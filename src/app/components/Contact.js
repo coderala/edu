d21c3e0f75b0c0927e60b17f105b517d1b8396ec
codershahzad <connectshahzadon@gmail.com>
2025-05-13 13:06:20 +0500

    alert page

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -6,7 +6,7 @@ import { faEnvelope, faBell, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const ContactSection = () => {
   return (
-    <div className="flex flex-col lg:flex-row m-5 bg-light p-5 lg:p-0 rounded-lg">
+    <div className="flex flex-col lg:flex-row m-5 bg-light p-5 lg:p-0 rounded-lg  lg:rounded-2xl">
       {/* Left Image Side */}
       <div className="w-full lg:w-1/2 bg-dark rounded-lg flex items-center justify-center relative p-5">
         {/* <FontAwesomeIcon
@@ -17,7 +17,7 @@ const ContactSection = () => {
       </div>
 
       {/* Right Form Side */}
-      <div className="w-full lg:w-1/2 flex flex-col justify-center mt-8 lg:mt-0 lg:pl-10 text-center lg:text-center gap-5 lg:p-20 ">
+      <div className="w-full lg:w-1/2 flex flex-col justify-center mt-8 lg:mt-0 lg:pl-10 text-center lg:text-center lg:p-20 ">
         <FontAwesomeIcon
           icon={faXmark}
           className="absolute top-8 right-8 text-black text-xl cursor-pointer"
@@ -26,7 +26,7 @@ const ContactSection = () => {
           Enjoy 15% Off Your First Order
         </h1>
 
-        <p className="mt-2 text-base sm:text-xl lg:text-sm 2xl:text-base text-gray-700">
+        <p className="mt-2 text-base sm:text-xl lg:text-lg font-bold 2xl:text-base text-gray-700">
           + free shipping & returns
         </p>
 
@@ -44,9 +44,10 @@ const ContactSection = () => {
           </div>
 
           <button className="mt-4 bg-yellow  font-poppins font-semibold py-3 px-6 rounded-lg text-base flex items-center gap-2 lg:w-10/12">
-            Sign Up Now
+            Get Code
             <FontAwesomeIcon icon={faBell} />
           </button>
+          <p className="mt-3">By entering your email, you agree to our Terms of Service + privacy Notice, including receipt of emails and promotions.You can unsubscribe at any time.</p>
         </div>
       </div>
     </div>
